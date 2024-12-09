9ac623e2ab5e6f436b767a9faaafdb84776fab63
aryanagrawal10 <158844278+aryanagrawal10@users.noreply.github.com>
2024-12-09 15:28:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// HINTS:
-// 1. Import express and axios
-
-// 2. Create an express app and set the port number.
-
-// 3. Use the public folder for static files.
-
-// 4. When the user goes to the home page it should render the index.ejs file.
-
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
-
-// 6. Listen on your predefined port and start the server.
-
 import express from 'express'
 import axios from 'axios'
 import bodyParser from 'body-parser'
@@ -20,11 +6,6 @@ const app = express()
 const port = 3000
 const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
-//Add your own bearer token from the previous lesson.
-// const yourBearerToken = '08f3026d-9c6c-4d88-a3b2-c579dc106247'
-// const config = {
-//   headers: { Authorization: `Bearer ${yourBearerToken}` },
-// }
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
